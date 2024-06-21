6e0d91c422e5ab526b777c67b9987e6bdf0256fd
Peter Baum <cloaknsmoke@gmail.com>
2024-06-21 00:46:14 -0500

    Add all magic using classes to the healer list. If you randomize magic, you might end up with a mage that starts with heal. No exp without this change.

--- disasm/data/stats/allies/classes/healerclasses-standard.asm
+++ disasm/data/stats/allies/classes/healerclasses-standard.asm
@@ -6,5 +6,17 @@ table_HealerClasses:
                 dc.b CLASS_PRST
                 dc.b CLASS_VICR
                 dc.b CLASS_MMNK
+				dc.b CLASS_MAGE_1
+				dc.b CLASS_MAGE_2
+				dc.b CLASS_MAGE_3
+				dc.b CLASS_MAGE_4
+				dc.b CLASS_SORC_1
+				dc.b CLASS_SORC_2
+				dc.b CLASS_SORC_3
+				dc.b CLASS_SORC_4
+				dc.b CLASS_WIZ
+				dc.b CLASS_SDMN
+				dc.b CLASS_HERO
+				dc.b CLASS_NINJ
                 
                 tableEnd.b
