4f2a8fd08e145793b4d67e231e610805ea9e9b0a
Peter Baum <cloaknsmoke@gmail.com>
2024-06-26 18:26:01 -0500

    Made some changes to how the patching worked since it was having trouble overwriting existing files.

--- split/patch_icon.bat
+++ split/patch_icon.bat
@@ -1,4 +1,6 @@
 @echo off
 CHCP 65001
-.\xdelta3_x86_64_win.exe -f -v -d -s "..\disasm\data\graphics\icons\icon061.bin" ".\vcdiff\icon061.bin.vcdiff" "..\disasm\data\graphics\icons\icon061.bin"
+mkdir "..\disasm\data\graphics\icons\output"
+.\xdelta3_x86_64_win.exe -f -v -d -s "..\disasm\data\graphics\icons\icon061.bin" ".\vcdiff\icon061.bin.vcdiff" "..\disasm\data\graphics\icons\output\icon061.bin"
+move "..\disasm\data\graphics\icons\output\icon061.bin" "..\disasm\data\graphics\icons\icon061.bin"
 echo Completed!
